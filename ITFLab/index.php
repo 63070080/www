218b8ff001bf04cd5dad1e8995f4aca6f1b13d22
63070080 <73686422+63070080@users.noreply.github.com>
2020-11-19 19:47:44 +0700

    Add files via upload

--- ITFLab/index.php
+++ ITFLab/index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
 		.card {
-			border-top: solid 5px #ffc107;
+			border-top: solid 5px #ff66ff;
 		}
 	</style>
 </head>
@@ -18,7 +18,7 @@
 					<div class="card-body">
 						<div class="row">
 							<div class="col-6"><h1 class="text-monospace">My Data</h1></div>
-							<div class="col-6 text-right"><a href="form.php" class="btn btn-warning btn-sm font-weight-bold">ADD</a></div>
+							<div class="col-6 text-right"><a href="form.php" class="btn btn-success btn-sm font-weight-bold">ADD</a></div>
 						</div>
 						<div class="table-responsive">
 							<?php
