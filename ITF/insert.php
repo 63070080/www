a59786510151bb31d3ae9afb6b1eea5f747c3160
63070080 <73686422+63070080@users.noreply.github.com>
2020-11-27 14:26:13 +0700

    Add files via upload

---
+++ ITF/insert.php
@@ -0,0 +1,48 @@
+<?php
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, 'data-tanapong.mysql.database.azure.com', 'nutgod@data-tanapong', 'nut7godalot9?', 'ITFLab', 3306);
+	if(mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+	$product = $_POST['product'];
+	$price = $_POST['price'];
+    $discount = $_POST['discount'];
+    $total = $price - $price*$discount/100
+	$sql = "INSERT INTO product (Product, Price, Discount, Total) VALUES ('$product', '$price', '$discount', '$total')";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADD</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #ff66ff;
+		}
+	</style>
+</head>
+<body class="bg-info py-5">
+	<div class="container bg-dark text-info">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "ADD SUCCESS";
+							}
+							else {
+								echo "FAILED TO ADD";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-sm btn-success">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ ITF/show.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Database</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #ff66ff;
+		}
+	</style>
+</head>
+<body class="bg-info py-5">
+	<div class="container bg-dark text-info">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-6"><h1 class="text-monospace">My Data</h1></div>
+							<div class="col-6 text-right"><a href="form.php" class="btn btn-success btn-sm font-weight-bold">ADD</a></div>
+						</div>
+						<div class="table-responsive">
+							<?php
+								$conn = mysqli_init();
+								mysqli_real_connect($conn, 'data-tanapong.mysql.database.azure.com', 'nutgod@data-tanapong', 'nut7godalot9?', 'ITFLab', 3306);
+								if (mysqli_connect_errno($conn))
+								{
+								    die('Failed to connect to MySQL: '.mysqli_connect_error());
+								}
+								$res = mysqli_query($conn, 'SELECT * FROM product');
+							?>
+							<table class="table table-borderless table-hover">
+								<thead class="thead-dark">
+									<tr>
+										<th width="150px">Action</th>
+										<th>Product</th>
+										<th>Price</th>
+                                        <th>Discount</th>
+                                        <th>Total</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+										while($Result = mysqli_fetch_array($res)) {
+									?>
+									<tr class="table-info">
+										<a href="edit.php?ID=<?php echo $Result['ID'];?>" class="btn btn-sm btn-dark">EDIT</a></td>
+										<td><?php echo $Result['Product'];?></td>
+										<td><?php echo $Result['Price'];?></td>
+                                        <td><?php echo $Result['Discount'];?></td>
+                                        <td><?php echo $Result['Total'];?></td>
+									</tr>
+									<?php
+										}
+									?>
+								</tbody>
+							</table>
+							<?php
+								mysqli_close($conn);
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
